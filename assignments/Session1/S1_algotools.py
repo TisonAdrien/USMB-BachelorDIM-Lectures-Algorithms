c8962beda88e5c16a1353aa7fdfab82069d74df7
TisonAdrien <adrienbogalski@gmail.com>
2017-10-16 11:45:41 +0200

    update condition

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -189,7 +189,7 @@ def random_fill_sparse(table,vfill):
 
 ##Sixth exercice
 def remove_whitespace(string):
-    if(isinstance(string,basestring)):
+    if(not isinstance(string,basestring)):
         raise Exception('Provided string is not a string')
     lenght = len(string)
     if lenght == 0:
@@ -216,7 +216,7 @@ def shuffle(liste):
     if len(liste) == 0:
         #Exception
         raise Exception('Provided list is empty')
-    if(isinstance(liste, (list, tuple)) ):
+    if(not isinstance(liste, (list, tuple)) or isinstance(liste,basestring)):
         raise Exception('Provided list is not a list')
     second_list = liste[:]
     new_list = []
@@ -269,7 +269,7 @@ def sort_selective(liste):
     if len(liste) == 0:
         #Exception
         raise Exception('Provided list is empty')
-    if(isinstance(liste, (list, tuple)) ):
+    if(not isinstance(liste, (list, tuple)) or isinstance(liste,basestring)):
         raise Exception('Provided list is not a list')
     for i in xrange(len(liste) - 1):
         min = i
@@ -342,7 +342,7 @@ def sort_bubble(liste):
     if len(liste) == 0:
         #Exception
         raise Exception('Provided list is empty')
-    if(isinstance(liste, (list, tuple)) ):
+    if(not isinstance(liste, (list, tuple)) or isinstance(liste,basestring)):
         raise Exception('Provided list is not a list')
     for i in xrange(0, len(liste) - 1):
         for x in xrange(0, len(liste) - 1):
